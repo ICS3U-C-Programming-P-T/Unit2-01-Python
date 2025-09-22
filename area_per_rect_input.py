e0dba2b3d30b4da0864082b4b1fe1915bd2c66ab
Patrick Student <patrick.tumbaga@stu.ocsb.ca>
2025-09-22 18:04:12 +0000

    area_per_rect_input.py

---
+++ area_per_rect_input.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Created By: Patrick
+# Date: 9/17/2025
+# My Area & Perimeter of Rectangle, in Python
+
+
+def main():
+    print("For the rectangle with a length of 67cm and a width of 78cm")
+    print("The area is {}cm^2".format(67 * 78))
+    print("The perimeter is: {}cm2^".format(2 * (67 + 78)))
+
+
+if __name__ == "__main__":
+    main()
